bf3893c1c996ab3eae106c2943fcb6a04a131fd3
Lumiin0us <73905501+Lumiin0us@users.noreply.github.com>
2023-06-18 14:49:38 +0500

    more comments lol

--- containsDuplicate.py
+++ containsDuplicate.py
@@ -1,7 +1,7 @@
 #https://leetcode.com/problems/contains-duplicate/
 
 class Solution:
-#! O(1) complexity 
+#! O(n) complexity (Best Case)
     def contains_duplicates(self, nums):
         length_before = len(nums)
         new_list = set(nums)
@@ -15,6 +15,5 @@ sol = Solution()
 sol.contains_duplicates([1,2,3,1])
 
 # other ways we could try to do this is in O(n)^2 complexity 
-#! Tried thinking if it is possible in O(n) and couldnt find any ideas. As searching each individual item def requires two loops for comparison
-#! some sort algorithms take less than O(n)^2 complexity we could use those as well and get some complexity between n^2 and 1 
+#! some sort algorithms take less than O(n)^2 complexity we could use those as well and get some complexity between n^2 and n
 # by running a nested forloop 
