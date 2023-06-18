40afb571ac322b230c39e0953140d1217c7adc57
Lumiin0us <73905501+Lumiin0us@users.noreply.github.com>
2023-06-18 14:45:14 +0500

    added comments

--- containsDuplicate.py
+++ containsDuplicate.py
@@ -16,4 +16,5 @@ sol.contains_duplicates([1,2,3,1])
 
 # other ways we could try to do this is in O(n)^2 complexity 
 #! Tried thinking if it is possible in O(n) and couldnt find any ideas. As searching each individual item def requires two loops for comparison
+#! some sort algorithms take less than O(n)^2 complexity we could use those as well and get some complexity between n^2 and 1 
 # by running a nested forloop 
