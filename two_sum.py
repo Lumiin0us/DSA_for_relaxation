d2f9d030d96a5417ea508542f58901aa8e9c7751
Lumiin0us <73905501+Lumiin0us@users.noreply.github.com>
2023-06-21 16:16:03 +0500

    two sum leetcode

--- two_sum.py
+++ two_sum.py
@@ -2,24 +2,23 @@
 
 class Solution:
     def two_sum(self, nums, target):
-        l = [2, 3, 1, 6, 1]
-        new_dict = {item: [] for item in l}
-        counter = 0 
-        while target > 0:
-            if counter >= len(new_dict):
-                counter = 0 
-            target = target - list(new_dict.keys())[counter]
-            new_dict[l[counter]].append(list(new_dict.keys())[counter])
-            counter += 1 
-        print(new_dict)
-
+        index_keeper = {}
+        l = [3,3]
+        for index, value in enumerate(nums):
+            num = target - value
+            if num in index_keeper:
+                return [index, index_keeper[num]]
+            index_keeper[value] = index
 
 sol = Solution()
 
-sol.two_sum(5, 10)
 
+solution = sol.two_sum([2, 7, 11, 15], 9)
+
+print(solution)
 # first of all we can remove all the stuff that is greater than our target! 
 # check all the combinations that can form the target element 
 # [2, 3, 1, 6] , target = 9 
+#! THE ABOVE APPROACH DIDNT WORK FOR ME SO WE THEN LEARNED ABOUT THE DICTIONARY APPROACH 
 
 
