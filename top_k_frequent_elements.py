ebc5e3649b9464b858dba4d2c5e5b8918c8c0e02
Lumiin0us <73905501+Lumiin0us@users.noreply.github.com>
2023-06-24 18:47:00 +0500

    leetcode

---
+++ top_k_frequent_elements.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+nums = []
+k = 2
+l = []
+dictionary = defaultdict(int)
+for num in nums:
+    dictionary[num] += 1
+sorted_dict = (sorted(dictionary.items(), key=lambda x: x[1]))
+for i in range(1, k + 1):
+    if len(nums) > 0:
+        l.append((list(sorted_dict[len(sorted_dict) - i])[0]))
+print(l)
