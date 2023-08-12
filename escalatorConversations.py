a15c85bbacba192a102db960472bfa21f6f7e32b
Lumiin0us <73905501+Lumiin0us@users.noreply.github.com>
2023-08-12 23:23:07 +0500

    ..

--- escalatorConversations.py
+++ escalatorConversations.py
@@ -1,5 +1,23 @@
 # https://codeforces.com/contest/1851/problem/A
 
+# 'n' passengers on the escalator 
+# 'm' steps of escalator 
+# '1 to m' total steps 
+# i-th step has height i.k 
+# vlad's height is 'H' centimeters 
+# height difference between people == height difference between the steps [170, 180, m = 10, k = 5]
 t = 1 
 while t: 
-    pass
+    n, m, k, H = input().split()
+    n, m, k, H = int(n), int(m), int(k), int(H)
+    heights = input().split()
+    heights = [int(height) for height in heights]
+    new_heights = heights
+    count = 0
+    for i in range(m - 1): 
+        for height in heights:
+            height += k
+            new_heights.append(height)
+        k += k 
+    print(new_heights)
+    t -= 1
